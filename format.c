03b94e000b739424dc276222dc3d2007b99dbe91
kevinasemota <kevinasemota@DESKTOP-MQKTK10>
2023-05-08 16:06:19 +0200

    fügt format.c hinzu

---
+++ format.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+int i = 7;
+float f = 7.7;
+char c = X;
+char s [] = "Hello World";
+
+    printf("Ganzzahl:%d\n",i);
+    printf("Gleitkommazahl:%f\n",f);
+    printf("Character:%c\n",c);
+    printf("String:%s\n",s);
+
+
+    return 0;
+
+}
